38abbbf6bd3f204115729623ac1131b2756a1628
Giovanni Crabi <crabigiovanni@gmail.com>
2022-12-13 00:39:35 +0000

    Created Section8

--- src/App.tsx
+++ src/App.tsx
@@ -7,6 +7,7 @@ import { Section4 } from './sections/Section4';
 import { Section5 } from './sections/Section5';
 import { Section6 } from './sections/Section6';
 import { Section7 } from './sections/Section7';
+import { Section8 } from './sections/Section8';
 
 function App() {
   
@@ -25,6 +26,8 @@ function App() {
       <Section6 />
 
       <Section7 />
+
+      <Section8 />
 
     </div>
   )
